8eda657968cff4479bc7f560a99f720474f84872
imsherr <imsherrill@gmail.com>
2024-12-28 19:11:58 -0500

    more

--- my-app/pages/_app.tsx
+++ my-app/pages/_app.tsx
@@ -1,8 +1,15 @@
 import "@/styles/globals.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import type { AppProps } from "next/app";
+
+const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
   const getLayout = Component.getLayout || ((page) => page);
 
-  return getLayout(<Component {...pageProps} />);
+  return (
+    <QueryClientProvider client={queryClient}>
+      {getLayout(<Component {...pageProps} />)}
+    </QueryClientProvider>
+  );
 }
--- my-app/pages/async-heavy-dom.tsx
+++ my-app/pages/async-heavy-dom.tsx
@@ -1,26 +1,37 @@
-import { useEffect, useState } from "react";
+import { useEffect, useId, useState } from "react";
 import { Layout } from "../components/Layout";
+import { useQuery } from "@tanstack/react-query";
 
 export default function VeryHeavyDOM() {
-  const [length, setLength] = useState(1);
-
-  useEffect(() => {
-    // Example effect that could change the length state
-    const timer = setTimeout(() => {
-      setLength(1e5); // Change the length after 5 seconds
-    }, 100);
-
-    return () => clearTimeout(timer);
-  }, []);
+  const id = useId();
+  const { isLoading, data } = useQuery({
+    queryKey: ["heavy-dom-async", id],
+    queryFn: async () => {
+      return 1e5;
+    },
+    initialData: 1,
+  });
 
   return (
     <div>
       <h1>Very Heavy DOM</h1>
-      <div>
-        {Array.from({ length }, (_, i) => (
-          <div key={i}>Element {i}</div>
-        ))}
-      </div>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <p>
+          Not loading. The number of elements to render is{" "}
+          {data.toLocaleString()}
+        </p>
+      )}
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <div>
+          {Array.from({ length: data }, (_, i) => (
+            <div key={i}>Element {i}</div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
